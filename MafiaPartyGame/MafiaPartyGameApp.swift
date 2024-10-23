9f7bbab9b14cde6db369cda8b1a6238c3dfd61bb
Dominik <domcio.tomcio@gmail.com>
2024-10-23 15:42:46 +0200

    Initial Commit

---
+++ MafiaPartyGame/MafiaPartyGameApp.swift
@@ -0,0 +1,32 @@
+//
+//  MafiaPartyGameApp.swift
+//  MafiaPartyGame
+//
+//  Created by Dominik Tomaszewski on 23/10/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MafiaPartyGameApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
