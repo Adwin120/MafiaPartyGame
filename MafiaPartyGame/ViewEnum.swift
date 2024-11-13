ba01fc47d90870a98a9eb6da1488e0377bd0490c
stud <stud@W04-M1MAC130-14.local>
2024-11-13 15:43:01 +0100

    fix stack ordering

---
+++ MafiaPartyGame/ViewEnum.swift
@@ -0,0 +1,20 @@
+//
+//  ViewEnum.swift
+//  MafiaPartyGame
+//
+//  Created by stud on 13/11/2024.
+//
+
+import Foundation
+
+
+// here we add main user paths
+
+enum ViewEnum {
+    case Play
+    case Characters
+    case Fraction
+    case User
+    case AppInfo
+    case Settings
+}
