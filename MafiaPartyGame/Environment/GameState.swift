4a79b84450b01ee5e0da8bcf1844b533f4888ed7
stud <stud@W04-M1MAC130-13.local>
2024-12-18 16:35:08 +0100

    add some business methods

--- MafiaPartyGame/Environment/GameState.swift
+++ MafiaPartyGame/Environment/GameState.swift
@@ -8,10 +8,34 @@
 import Observation
 
 @Observable class GameState {
-    var players: [Player] = []
+    var players: [String: Player] = [:]
+    var phase: Phase = .Day0
     
     var winner: Optional<Fraction> = Optional.none
     var inProgress: Bool {
         winner == Optional.none
     }
+    
+    func kill(playerId: String) {
+        self.players[playerId]?.isAlive = false
+    }
+    
+    func heal(playerId: String) -> Bool {
+        var player = self.players[playerId].unsafelyUnwrapped
+        var wasAlive = player.isAlive
+        player.isAlive = true
+        return !wasAlive
+    }
+    
+    func newTurn() {
+        self.phase = self.phase.next
+        var countMafia = players.filter {key, player in player.character.fraction == .Mafia}.count
+        if countMafia > players.count / 2 {
+            self.winner = .Mafia
+        }
+        if countMafia == 1 && players.count == 2 {
+            self.winner = .Mafia
+        }
+    }
+    
 }
