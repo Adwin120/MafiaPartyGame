a0f706216e54eaceae08ca623a5f1b605d170569
Adwin120 <adam.winnik120@gmail.com>
2025-01-22 15:33:48 +0100

    implement unit tests

--- MafiaPartyGameTests/MafiaPartyGameTests.swift
+++ MafiaPartyGameTests/MafiaPartyGameTests.swift
@@ -19,34 +19,54 @@ struct MafiaPartyGameTests {
         
     }
     @Test func doctorCantHealExecutedPlayer() async throws {
-        
+        let gameState = GameState()
+        #expect(gameState.execute(playerId: "1"))
+        #expect(gameState.players["1"] == Some.none) // expect not to be there
+        #expect(!gameState.heal(playerId: "1"))
     }
     @Test func doctorCantHealInNextTurn() async throws {
+        let gameState = GameState()
+
+        gameState.assassinate(playerId: "1")
+        gameState.newTurn()
         
+        #expect(gameState.players["1"] == Optional.none) // expect not to be there
+        #expect(!gameState.heal(playerId: "1"))
     }
-    @Test func cattanisprawdzamafiejakomafia() async throws {
+    // @Test func cattanisprawdzamafiejakomafia() async throws {
         
+    // }
+    // @Test func cattanimiasto() async throws {
+        
+    // }
+    @Test func thePlayerDiesWhenDoctorMisses() async throws {
+        let gameState = GameState()
+        #expect(gameState.assassinate(playerId: "1"))
+        #expect(gameState.players["1"]?.status == .RecentlyDeceased)
+        #expect(gameState.heal(playerId: "2"))
+        gameState.nextTurn()
+        #expect(gameState.players["1"]?.status == .Deceased)
     }
-    @Test func cattanimiasto() async throws {
-        
+    @Test func winConditionOneMafiaOneTown() async throws {
+        let gameState = GameState()
+        gameState.execute(playerId: "2")
+        gameState.nextTurn()
+        #expect(gameState.winner = Optional.some(.Mafia))
     }
-    @Test func jakmafiastrzelailekarznietrafitoumiera() async throws {
-        
+    @Test func winConditionZeroMafia() async throws {
+        let gameState = GameState()
+        gameState.execute(playerId: "1")
+        gameState.nextTurn()
+        #expect(gameState.winner == Optional.some(.Town))
     }
-    @Test func jaktasamaosobatoprzezywa() async throws {
+    @Test func winConditionMoreMafiaThanTown() async throws {
+        let gameState = GameState()
+        gameState.addPlayer("mafia crook", Character(id: 1, name: "Mafia Boss", descriptionText: "lorem ipsum", fraction: .Mafia, image: "mafiaBoss"))
+        gameState.addPlayer("mafia crook2", Character(id: 1, name: "Mafia Boss", descriptionText: "lorem ipsum", fraction: .Mafia, image: "mafiaBoss"))
         
-    }
-    @Test func jakmiastoglosuje() async throws {
-        
-    }
-    @Test func jakjedenmafiajedenmiasta() async throws {
-        
-    }
-    @Test func jedenmiastazeromafii() async throws {
-        
-    }
-    @Test func mafiistrictewiecejnizmiasta() async throws {
-        
+        gameState.nextTurn()
+
+        #expect(gameState.winner == Optional.some(.Mafia))
     }
 
 }
